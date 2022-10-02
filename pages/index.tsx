74dd15a969f73538405ca7f69c568e444897c15b
cheeter_Lee <johnson991210@gmail.com>
2022-10-02 22:08:12 +0800

    set up nextjs with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Cheeter's Portfolio</title>
+      </Head>
+      <h1 className='text-red-500'>hello world</h1>
+    </div>
+  )
+}
+
+export default Home
