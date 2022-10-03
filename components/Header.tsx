60465866576bb9d18f4cf386af10cd8bc6bb2ee5
cheeter_Lee <johnson991210@gmail.com>
2022-10-03 11:32:41 +0800

    add animation hero section background

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
+      <motion.div 
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className='flex flex-row items-center'
+      >
         <SocialIcon 
           url="https://github.com/cheeterLee" 
           fgColor='gray'
@@ -22,9 +38,24 @@ const Header = (props: Props) => {
           fgColor='gray'
           bgColor='transparent'
         />
-      </div>
+      </motion.div>
 
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div 
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+      >
         <SocialIcon 
           className='cursor-pointer'
           network='email'
@@ -32,7 +63,7 @@ const Header = (props: Props) => {
           bgColor='transparent'
         />
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
-      </div>
+      </motion.div>
     </header>
   )
 }
