266e9d265c410c1d84ccb28d9274fadf68b8c014
cheeter_Lee <johnson991210@gmail.com>
2022-10-03 10:11:27 +0800

    implement react social icons

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+      <div className='flex flex-row items-center'>
+        <SocialIcon 
+          url="https://github.com/cheeterLee" 
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon 
+          url="https://github.com/cheeterLee" 
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon 
+          url="https://github.com/cheeterLee" 
+          fgColor='gray'
+          bgColor='transparent'
+        />
+      </div>
+
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <SocialIcon 
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +8,20 @@ const Home: NextPage = () => {
       <Head>
         <title>Cheeter's Portfolio</title>
       </Head>
-      <h1 className='text-red-500'>hello world</h1>
+      
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* skills */}
+
+      {/* projects */}
+
+      {/*  */}
     </div>
   )
 }
