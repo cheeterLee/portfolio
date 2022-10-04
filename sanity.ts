b24da00f7e3422f020b7e962e08247e7cc52bcbe
cheeter_Lee <johnson991210@gmail.com>
2022-10-04 20:03:22 +0800

    fix bug in sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJCET_ID!,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production"
 }
