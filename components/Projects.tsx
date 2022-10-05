cc718540e5906d278917d375a11dff4911a2b270
cheeter_Lee <johnson991210@gmail.com>
2022-10-05 16:41:30 +0800

    add key prop in projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,7 @@ const Projects = ({projects}: Props) => {
 
             <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
                 {projects?.map((project, i) => (
-                    <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+                    <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                         <motion.img
                             initial={{
                                 y: -300,
